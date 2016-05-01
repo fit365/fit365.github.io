e43c1480d6ec79d12fecf4aa1001dbec8505f6e9
cerinman <tommy.dugger78@gmail.com>
2016-04-30 21:43:55 -0600

    add proxima nova font and revamp normalize css

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,14 @@ const Header = React.createClass({
 
     return (
       <div style={styles.component}>
-
+        <div style={styles.title}>
+          <span style={styles.fit}>
+            FIT
+          </span>
+          <span style={styles.threeSixtyFive}>
+            365
+          </span>
+        </div>
       </div>
     );
   },
@@ -16,9 +23,24 @@ const Header = React.createClass({
   styles () {
     return {
       component: {
-        backgroundColor: StyleConstants.Colors.PRIMARY,
-        height: 450,
+        backgroundColor: StyleConstants.Colors.DARKGREY,
+        display: 'flex',
+        height: 550,
+        justifyContent: 'center',
         width: '100%'
+      },
+      fit: {
+        color: StyleConstants.Colors.PORCELAIN,
+        fontFamily: StyleConstants.Fonts.SEMIBOLD,
+        fontSize: 45
+      },
+      threeSixtyFive: {
+        color: StyleConstants.Colors.PORCELAIN,
+        fontFamily: StyleConstants.Fonts.NORMAL,
+        fontSize: 30
+      },
+      title: {
+        padding: 100
       }
     }
   }
